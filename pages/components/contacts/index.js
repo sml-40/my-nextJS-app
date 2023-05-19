6bd213a80331055faa6daeed735ea4fdd9ea1daf
Selina Lavery <selina118@hotmail.com>
2023-05-19 11:34:08 +0100

    Refactor of contacts-index.js, can now see contacts.name on wep page components/contacts

--- pages/components/contacts/index.js
+++ pages/components/contacts/index.js
@@ -1,20 +1,14 @@
 import Link from "next/link";
-import contacts from "@/pages/api/contacts";
-import Layout from "../layout";
+import contacts from "pages/api/contacts.js";
 
 export default function Contacts() {
 	return (
-		<Layout>
-			<h1>Contacts</h1>
+		<>
 			<ul>
-				{contacts.map((contact) => (
-					<li key={contact.id}>
-						<Link href={`/contacts/${contact.id}`}>
-							<a>{contact.name}</a>
-						</Link>
-					</li>
-				))}
+				{contacts.map((contact) => {
+					return <li> {contact.name}</li>;
+				})}
 			</ul>
-		</Layout>
+		</>
 	);
 }
